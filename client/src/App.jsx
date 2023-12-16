a424272a8d25148d92bcfe21a16f6f494e690fd3
unawarexi <unawarexi@gmail.com>
2023-12-16 01:58:04 +0100

    Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {LoginMain, Login} from "./containers/LoginExport";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className=" w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      <Routes>
+        <Route path="/#" element={<LoginMain />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
