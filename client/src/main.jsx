37700ff3e1e03f533dd6ef06dd061766cb5dd8f1
unawarexi <unawarexi@gmail.com>
2023-12-16 01:58:48 +0100

    Framer-motion, Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <AnimatePresence>
+        <App />
+      </AnimatePresence>
+    </Router>
+  </React.StrictMode>
+);
