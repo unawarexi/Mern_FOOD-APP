c07c6737e08dbd46ac8d524c8ca850efca03860a
unawarexi <unawarexi@gmail.com>
2023-12-22 03:23:18 +0100

    Server file

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,19 +1,37 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+require("dotenv").config();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const serviceAccountKey = require("./serviceAccountKey.json");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const express = require("express");
+const app = express();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// body parser for json data
+
+app.use(express.json());
+
+// cors origin
+const cors = require("cors");
+app.use(cors({ origin: true }));
+
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  next();
+});
+
+//  FIREBASE CREDENTIALS
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+// API END POINTS
+app.get("/", (req, res) => {
+  return res.send("hello world");
+});
+
+const UserRoutes = require("./routes/User")
+app.use("/api/users", UserRoutes)
+
+exports.app = functions.https.onRequest(app);
