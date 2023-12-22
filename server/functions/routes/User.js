847fe845399c6a06f236e81a56f392d6601cf6d3
unawarexi <unawarexi@gmail.com>
2023-12-22 03:23:54 +0100

    User info Security

---
+++ server/functions/routes/User.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const admin = require("firebase-admin");
+
+router.get("/", (req, res) => {
+  return res.send("Inside the user router");
+});
+
+router.get("/jwtVerification", async (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(500).send({ msg: "token not found" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const decodedValue = await admin.auth().verifyIdToken(token);
+    if (!decodedValue) {
+      return res
+        .status(500)
+        .json({ success: false, msg: "Unauthorised access" });
+    }
+
+    return res.status(200).json({success : true, data : decodedValue})
+
+
+  } catch (error) {
+    return res.send({
+      success: false,
+      msg: `Error Extracting Token: ${error}`,
+    });
+  }
+});
+
+module.exports = router;
