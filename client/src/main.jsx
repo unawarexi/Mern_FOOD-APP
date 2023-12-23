1632cfd318e7418fc1b88ad1fc7080a8775bd7d7
unawarexi <unawarexi@gmail.com>
2023-12-23 09:18:06 +0100

    Creating Redux Store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,22 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import myReducers from "./context/reducers/ReducersIndex.jsx";
+
+const myStore = createStore(
+  myReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); // helps monitor whatever is happeining in our store
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <AnimatePresence>
-        <App />
+        <Provider store={myStore}>
+          <App />
+        </Provider>
       </AnimatePresence>
     </Router>
   </React.StrictMode>
