0b9d5191e74a32f706146fd8d6fe77999f466d42
unawarexi <unawarexi@gmail.com>
2023-12-26 18:29:47 +0100

    Added Slidding Animations

--- client/src/animations/Animation.jsx
+++ client/src/animations/Animation.jsx
@@ -7,3 +7,9 @@ export const FadeInOut = {
   animate: { opacity: 1 },
   exit: { opacity: 0 },
 };
+
+export const slideTop = {
+  initial: { opacity: 0, y: 30 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 30 },
+};
