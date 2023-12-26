ca1ba4970d1ca2b976d04c71eee076222adfc1b0
unawarexi <unawarexi@gmail.com>
2023-12-26 18:33:06 +0100

    Updated

--- client/src/components/ExpComp.jsx
+++ client/src/components/ExpComp.jsx
@@ -1,3 +1,16 @@
 export {default as LoginInput} from "./LoginInput"
 export {default as MainLoader} from "./MainLoader"
 export {default as Alert} from "./Alert"
+export {default as Header} from "./Header"
+
+
+export {default as DBRight} from "./DBRight"
+export {default as DBLeft} from "./DBLeft"
+export {default as DBHeader} from "./DBHeader"
+
+
+export {default as DBHome} from "./DBHome"
+export {default as DBUsers} from "./DBUsers"
+export {default as DBOrders} from "./DBOrders"
+export {default as DBItems} from "./DBItems"
+export {default as DBNewItems} from "./DBNewItems"
