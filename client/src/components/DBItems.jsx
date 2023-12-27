a533fd682395b7983e14f855182cfc4e65ef5465
unawarexi <unawarexi@gmail.com>
2023-12-27 03:02:51 +0100

    Mern-FOOD-APP

--- client/src/components/DBItems.jsx
+++ client/src/components/DBItems.jsx
@@ -1,52 +1,13 @@
-import React from 'react'
-import MaterialTable from "material-table"
-import {ThemeProvider, createTheme} from "@mui/material"
+import React from "react";
+
+import { DataTable } from "./ExpComp";
 
 const DBItems = () => {
-   const defaultTheme = createTheme()
-
-
-
   return (
-    <div className='flex items-center justify-self-center gap-4 pt-6 w-full '>
-       <ThemeProvider theme={defaultTheme}>
-       <MaterialTable
-      title="Remote Data Preview"
-      columns={[
-        {
-          title: 'Avatar',
-          field: 'avatar',
-          render: rowData => (
-            <img
-              style={{ height: 36, borderRadius: '50%' }}
-              src={rowData.avatar}
-            />
-          ),
-        },
-        { title: 'Id', field: 'id' },
-        { title: 'First Name', field: 'first_name' },
-        { title: 'Last Name', field: 'last_name' },
-      ]}
-      data={query =>
-        new Promise((resolve, reject) => {
-          let url = 'https://reqres.in/api/users?'
-          url += 'per_page=' + query.pageSize
-          url += '&page=' + (query.page + 1)
-          fetch(url)
-            .then(response => response.json())
-            .then(result => {
-              resolve({
-                data: result.data,
-                page: result.page - 1,
-                totalCount: result.total,
-              })
-            })
-        })
-      }
-    />
-       </ThemeProvider>
+    <div className="flex items-center justify-self-center gap-4 pt-6 w-full ">
+      <DataTable  />
     </div>
-  )
-}
+  );
+};
 
 export default DBItems;
--- client/src/components/DBNewItems.jsx
+++ client/src/components/DBNewItems.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { statuses } from "../utils/Styles";
 
 const DBNewItems = () => {
+  const [itemName, setitemName] = useState("");
+  const [Category, setCategory] = useState("");
   return (
-    <div>DBNewItems</div>
-  )
-}
+    <div className="flex items-center justify-center flex-col pt-6 px-24 w-full">
+      <div
+        className="border border-gray-300 rounded-md p-4 w-full flex flex-col items-center
+       justify-center gap-4"
+      >
+        <InputValueField
+          type="text"
+          placeholder={"Item name here"}
+          stateFunc={setitemName}
+          stateValue={itemName}
+        />
 
-export default DBNewItems
+        <div className="w-full flex items-center justify-around gap-3 flex-wrap ">
+          {statuses &&
+            statuses?.map((data) => (
+              <p
+                key={data.id}
+                onClick={() => setCategory(data.category)}
+                className={`px-4 py-3 rounded-md text-xl text-textColor
+                font-semibold cursor-pointer hover:shadow-md border border-gray-200 backdrop-blur-md
+                ${
+                  data.category === Category
+                    ? "bg-red-400 text-primary"
+                    : "bg-transparent"
+                }`}
+              >
+                {data.title}
+              </p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const InputValueField = ({
+  type,
+  placeholder,
+  stateValue,
+  stateFunc,
+}) => {
+  return (
+    <>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="w-full px-4 py-3 bg-cardOverlay shadow-md outline-none rounded-md border
+       border-gray-200 focus:border-red-400"
+        value={stateValue}
+        onChange={(e) => stateFunc(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default DBNewItems;
