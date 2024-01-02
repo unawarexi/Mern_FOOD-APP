3c35c15aa7827358c86debf1c2c843ad7d138cd0
unawarexi <unawarexi@gmail.com>
2024-01-02 16:07:09 +0100

    Updated

--- client/src/context/reducers/ReducersIndex.jsx
+++ client/src/context/reducers/ReducersIndex.jsx
@@ -1,10 +1,14 @@
 import {combineReducers} from "redux"
 import userReducer from "./UserReducer"
 import alertReducer from './AlertReducer.jsx';
+import productReducer from './ProductReducer.jsx';
+
 
 const myReducers = combineReducers({
     user : userReducer,
-    alert: alertReducer
+    alert: alertReducer,
+    products: productReducer,
+
 })
 
 export default myReducers;
