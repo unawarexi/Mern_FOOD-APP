51ab81c4a6169efa7b4a4b5710d7ba0f96a6a61f
unawarexi <unawarexi@gmail.com>
2024-01-03 00:30:05 +0100

    Router to Get All

--- server/functions/routes/Proudct.js
+++ server/functions/routes/Proudct.js
@@ -21,4 +21,29 @@ router.post("/create", async (req, res) => {
     return res.send({ success: false, msg: `Error : ${error}` });
   }
 });
+
+//GET ALL THE PRODUCTS
+router.get("/all", async (req, res) => {
+    (async () => {
+       try {
+
+        let query = db.collection('products')
+        let response = []
+        await query.get().then(querySnap => {
+          let docs = querySnap.docs 
+          docs.map(doc => {
+            response.push({...doc.data()})
+          })
+         return response;
+        })
+        
+        return res.status(200).send({ status: true, data: response });
+        
+       } catch (error) {
+        return res.send({ success: false, msg: `Error : ${error}` });
+        
+       }
+
+    })();
+})
 module.exports = router;
