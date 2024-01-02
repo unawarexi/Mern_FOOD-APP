7236de80dcdca69c62b974a2c1c304c2aa6c62cd
unawarexi <unawarexi@gmail.com>
2024-01-03 00:29:25 +0100

    display all in dboard home

--- client/src/components/DBHome.jsx
+++ client/src/components/DBHome.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllProducts } from '../api/IndexApi'
+import { setAllProucts } from '../context/actions/ProductAction'
 
 const DbHome = () => {
+ const products =  useSelector(state => state.products)
+
+ const dispatch = useDispatch();
+
+ useEffect(() => {
+   if(!products){
+    getAllProducts().then(data => {
+      dispatch(setAllProucts(data));
+      
+    })
+   }
+ }, [])
+ 
+
+
   return (
-    <div>DbHome</div>
+    <div>
+
+    </div>
   )
 }
 
