9495be648448076d28b3cf4bb9465987e2300d63
unawarexi <unawarexi@gmail.com>
2024-01-09 23:58:55 +0100

    Updated

--- client/src/containers/LoginExport.jsx
+++ client/src/containers/LoginExport.jsx
@@ -1,3 +1,3 @@
 export {default as LoginMain} from "./LoginMain"
 export {default as Login} from "./Login"
-export {default as DashBoard} from "./DashBoard"
+export {default as DashBoard} from './DashBoard'
